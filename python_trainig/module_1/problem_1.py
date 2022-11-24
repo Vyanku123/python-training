4a281f36142f5c649c0894961988042d32deb051
kingkhan123 <venkateshpandit863@gmail.com>
2022-11-24 18:30:07 +0530

    Change

--- python_trainig/module_1/problem_1.py
+++ python_trainig/module_1/problem_1.py
@@ -1,2 +1,3 @@
 print('Twinkle, twinkle, little star,\n How I wonder what you are.\n Up above the world so high,\n Like a diamond in the sky.\n')
-print("Hello world")
+print("Hello world vyanku")
+
